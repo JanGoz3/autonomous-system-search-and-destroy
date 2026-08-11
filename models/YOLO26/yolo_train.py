46e28aa2d740fa311674b22127572c31e8133f10
JanGoz3 <ckm7667@gmail.com>
2026-08-11 16:03:16 +0200

    CHANGE renamed yolo_training file

--- models/YOLO26/yolo_train.py
+++ models/YOLO26/yolo_train.py
@@ -5,9 +5,9 @@ model = YOLO('yolo26m.pt')
 if __name__ == '__main__':
     results = model.train(
         data='data/data.yaml', 
-        epochs=50,      
-        imgsz=640,       
+        epochs=2,      
+        imgsz=320,       
         batch=16,        
         device=0, 
-        name='drzwi_model_v1' # Nazwa folderu, w którym zapiszą się wyniki
+        name='YOLO26m_ours_smallImgs' # Nazwa folderu, w którym zapiszą się wyniki
     )
